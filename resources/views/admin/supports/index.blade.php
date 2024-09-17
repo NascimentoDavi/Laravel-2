6459b2d58cf3a8eee6a607bfaef66047938965cc
Davi Nascimento <davirnascimentox@gmail.com>
2024-09-17 16:59:06 -0300

    supportts page

---
+++ resources/views/admin/supports/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Supports Page</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\SupportController;
 use App\Http\Controllers\Site\SiteController;
+use App\Http\Controllers\Forum\ForumController;
 use Illuminate\Support\Facades\Route;
+
+// Forum development
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
 
 Route::get('/contact', [SiteController::class, 'contact']);
 
